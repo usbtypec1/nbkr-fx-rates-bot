ff8150719f7b47393763732f4b86545d1795f0ba
usbtypec1 <eldos.baktybekov@gmail.com>
2022-07-18 13:49:29 +0600

    :recycle: Use dataclasses instead of `NamedTuple`

--- src/data_types.py
+++ src/data_types.py
@@ -1,18 +1,21 @@
-from typing import NamedTuple
+from dataclasses import dataclass
 
 
-class CurrencyFXRate(NamedTuple):
+@dataclass(frozen=True, slots=True)
+class CurrencyFXRate:
     iso_code: str
     nominal: int
     value: float
 
 
-class NewAndOldFXRatesPair(NamedTuple):
+@dataclass(frozen=True, slots=True)
+class NewAndOldFXRatesPair:
     old: CurrencyFXRate
     new: CurrencyFXRate
 
 
-class MatchedCurrencies(NamedTuple):
+@dataclass(frozen=True, slots=True)
+class MatchedCurrencies:
     new_and_old_fx_rates_pairs: list[NewAndOldFXRatesPair]
     single_old_fx_rates: list[CurrencyFXRate]
     single_new_fx_rates: list[CurrencyFXRate]
