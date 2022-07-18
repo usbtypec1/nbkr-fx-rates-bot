3b6b7d37a9e9172663a8918618938e313e14c862
usbtypec1 <eldos.baktybekov@gmail.com>
2022-07-18 13:44:37 +0600

    :wrench: Update config file

--- src/config.py
+++ src/config.py
@@ -1,20 +1,28 @@
+import os
 import pathlib
 
-from environs import Env
+import exceptions
 
 __all__ = (
     'TELEGRAM_BOT_TOKEN',
     'TELEGRAM_CHAT_IDS',
     'DATABASE_PATH',
     'SRC_DIR',
+    'ROOT_PATH',
 )
 
-env = Env()
-env.read_env()
+TELEGRAM_BOT_TOKEN = os.getenv('TELEGRAM_BOT_TOKEN')
+_chat_ids = os.getenv('TELEGRAM_CHAT_IDS')
 
-TELEGRAM_BOT_TOKEN = env.str('TELEGRAM_BOT_TOKEN')
-TELEGRAM_CHAT_IDS = env.list('TELEGRAM_CHAT_IDS', subcast=int)
+if _chat_ids is None:
+    raise exceptions.ConfigError('Environment variable "TELEGRAM_CHAT_IDS" is not set')
+if TELEGRAM_BOT_TOKEN is None:
+    raise exceptions.ConfigError('Environment variable "TELEGRAM_BOT_TOKEN" is not set')
 
-SRC_DIR = pathlib.Path(__file__).parent
+TELEGRAM_CHAT_IDS = [int(chat_id) for chat_id in _chat_ids.split(',')]
+
+ROOT_PATH = pathlib.Path(__file__).parent.parent
+
+SRC_DIR = ROOT_PATH / 'src'
 
 DATABASE_PATH = SRC_DIR / 'database.db'
