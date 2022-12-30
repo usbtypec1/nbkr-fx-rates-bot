4ee5fadf2c9355512e4ff7eff4261e0b5f0ffd8a
Eldos <eldos.baktybekov@gmail.com>
2022-12-31 00:31:12 +0600

    :recycle: Refactor entrypoint

--- src/main.py
+++ src/main.py
@@ -1,32 +1,90 @@
+import logging
 import pathlib
+import traceback
+from typing import TypedDict, Callable, TypeVar
 
-import db
+from data_types import CurrencyFXRate
+from db import SqliteDatabase, upsert_currency_fx_rates, init_database, get_currency_fx_rates
+import exceptions
 
-import national_bank
 from config import load_config
-import utils
+from banks.parsers import parse_commercial_bank_fx_rates, parse_national_bank_fx_rates
+from banks.api import get_national_bank_fx_rates, get_commercial_bank_fx_rates
+from views import group_fx_rates_by_bank_name, render_fx_rates_delta_view
 from telegram import TelegramBot
+
+T = TypeVar('T')
+
+
+class Strategy(TypedDict):
+    api_callback: Callable[[], T]
+    parser_callback: Callable[[T], list[CurrencyFXRate]]
+
+
+strategies: tuple[Strategy, ...] = (
+    {
+        'api_callback': get_commercial_bank_fx_rates,
+        'parser_callback': parse_commercial_bank_fx_rates,
+    },
+    {
+        'api_callback': get_national_bank_fx_rates,
+        'parser_callback': parse_national_bank_fx_rates,
+    },
+)
 
 
 def main():
+    database_file_path = pathlib.Path(__file__).parent.parent / 'database.db'
     config_file_path = pathlib.Path(__file__).parent.parent / 'config.ini'
     config = load_config(config_file_path)
 
     telegram_bot = TelegramBot(config.telegram_bot_token)
-    db.init_database()
-    new_fx_rates = national_bank.parse_fx_rates_xml(national_bank.get_fx_rates())
-    old_fx_rates = db.get_currencies()
-    matched_currencies = utils.match_currencies(old_fx_rates, new_fx_rates)
+    database = SqliteDatabase(database_file_path)
+    currencies_fx_rates: list[CurrencyFXRate] = []
 
-    for new_fx_rate in matched_currencies.single_new_fx_rates:
-        db.insert_currency(new_fx_rate)
-    for fx_rate_pairs in matched_currencies.new_and_old_fx_rates_pairs:
-        if fx_rate_pairs.old.value == fx_rate_pairs.new.value:
+    for strategy in strategies:
+        api_callback = strategy['api_callback']
+        parser_callback = strategy['parser_callback']
+
+        try:
+            raw_fx_rates = api_callback()
+        except exceptions.BankAPIError:
+            logging.error('Could not load data from banks API')
             continue
-        text = utils.format_changed_fx_rate_text(fx_rate_pairs.old, fx_rate_pairs.new)
-        for chat_id in config.chat_ids:
-            telegram_bot.send_message(chat_id, text)
-        db.update_currency(fx_rate_pairs.new)
+
+        try:
+            currencies_fx_rates += parser_callback(raw_fx_rates)
+        except Exception:
+            # log all available exceptions to catch only them in the future
+            logging.error(traceback.format_exc())
+            continue
+
+    changed_currency_fx_rates = []
+    with database.closing_connection() as connection:
+        init_database(connection)
+        for new_currency_fx_rates in currencies_fx_rates:
+            try:
+                old_currency_fx_rates = get_currency_fx_rates(
+                    connection=connection,
+                    bank_name=new_currency_fx_rates.bank_name,
+                    iso_code=new_currency_fx_rates.iso_code,
+                )
+            except exceptions.FXRateNotFoundError:
+                logging.warning('FX rates not found in database')
+            else:
+                if new_currency_fx_rates == old_currency_fx_rates:
+                    continue
+                changed_currency_fx_rates.append((old_currency_fx_rates, new_currency_fx_rates))
+            finally:
+                upsert_currency_fx_rates(connection, currency_fx_rates=new_currency_fx_rates)
+
+    if not changed_currency_fx_rates:
+        return
+    grouped_currency_fx_rates = group_fx_rates_by_bank_name(changed_currency_fx_rates)
+    rendered_view = render_fx_rates_delta_view(grouped_currency_fx_rates)
+
+    for chat_id in config.chat_ids:
+        telegram_bot.send_message(chat_id, rendered_view)
 
 
 if __name__ == '__main__':
