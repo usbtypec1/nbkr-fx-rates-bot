4c1cfb4d20166c70c939abe6b53c5971c7284ddb
Eldos <eldos.baktybekov@gmail.com>
2022-12-31 00:30:52 +0600

    :card_file_box: Refactor database

--- src/db.py
+++ src/db.py
@@ -1,62 +1,81 @@
 import contextlib
 import pathlib
 import sqlite3
-from typing import Union
+from decimal import Decimal
 
-import config
+import exceptions
 from data_types import CurrencyFXRate
 
 __all__ = (
-    'insert_currency',
-    'update_currency',
-    'get_currencies',
+    'SqliteDatabase',
+    'get_currency_fx_rates',
     'init_database',
+    'upsert_currency_fx_rates',
 )
 
 
-@contextlib.contextmanager
-def auto_closing_database(database_path: Union[pathlib.Path, str]) -> sqlite3.Cursor:
-    """Provides cursor of sqlite database and safely closes it.
+class SqliteDatabase:
 
-    Args:
-        database_path: Path to database.
+    def __init__(self, database_file_path: str | pathlib.Path):
+        self.__database_file_path = database_file_path
 
-    Returns:
-        Cursor of database.
-    """
-    with (contextlib.closing(sqlite3.connect(database_path)) as connection,
-          connection,
-          contextlib.closing(connection.cursor()) as cursor):
-        yield cursor
+    @contextlib.contextmanager
+    def closing_connection(self) -> sqlite3.Connection:
+        connection = sqlite3.connect(self.__database_file_path)
+        with contextlib.closing(connection):
+            yield connection
 
 
-def insert_currency(fx_rate: CurrencyFXRate):
-    query = 'INSERT INTO currency_fx_rates (iso_code, nominal, value) VALUES (?,?,?);'
-    with auto_closing_database(config.DATABASE_PATH) as cursor:
-        cursor.execute(query, (fx_rate.iso_code, fx_rate.nominal, fx_rate.value))
-
-
-def update_currency(fx_rate: CurrencyFXRate):
-    query = 'UPDATE currency_fx_rates SET nominal=?, value=? WHERE iso_code=?;'
-    with auto_closing_database(config.DATABASE_PATH) as cursor:
-        cursor.execute(query, (fx_rate.nominal, fx_rate.value, fx_rate.iso_code))
-
-
-def get_currencies() -> list[CurrencyFXRate]:
-    query = 'SELECT iso_code, nominal, value FROM currency_fx_rates;'
-    with auto_closing_database(config.DATABASE_PATH) as cursor:
-        cursor.execute(query)
-        return [CurrencyFXRate(*row) for row in cursor.fetchall()]
-
-
-def init_database():
+def init_database(connection: sqlite3.Connection) -> None:
     query = '''
-    CREATE TABLE IF NOT EXISTS currency_fx_rates (
-        id INTEGER PRIMARY KEY AUTOINCREMENT
-      , iso_code TEXT NOT NULL UNIQUE
-      , nominal INTEGER NOT NULL
-      , value FLOAT NOT NULL
+    CREATE TABLE IF NOT EXISTS fx_rates (
+        bank_name TEXT NOT NULL,
+        iso_code TEXT NOT NULL,
+        buy_value TEXT NOT NULL,
+        sell_value TEXT NOT NULL,
+        UNIQUE (bank_name, iso_code) ON CONFLICT IGNORE
     );
     '''
-    with auto_closing_database(config.DATABASE_PATH) as cursor:
-        cursor.execute(query)
+    connection.execute(query)
+    connection.commit()
+
+
+def upsert_currency_fx_rates(connection: sqlite3.Connection, currency_fx_rates: CurrencyFXRate) -> None:
+    query = '''
+    INSERT INTO fx_rates (bank_name, iso_code, buy_value, sell_value) 
+        VALUES (?,?,?,?)
+    ON CONFLICT(bank_name, iso_code) DO 
+        UPDATE SET buy_value = ?, sell_value = ?
+        WHERE bank_name = ? AND iso_code = ?;
+    '''
+    params = (
+        currency_fx_rates.bank_name,
+        currency_fx_rates.iso_code,
+        str(currency_fx_rates.buy_value),
+        str(currency_fx_rates.sell_value),
+        str(currency_fx_rates.buy_value),
+        str(currency_fx_rates.sell_value),
+        currency_fx_rates.bank_name,
+        currency_fx_rates.iso_code,
+    )
+    cursor = connection.cursor()
+    with contextlib.closing(cursor):
+        cursor.execute(query, params)
+        connection.commit()
+
+
+def get_currency_fx_rates(connection: sqlite3.Connection, bank_name: str, iso_code: str) -> CurrencyFXRate:
+    query = 'SELECT bank_name, iso_code, buy_value, sell_value FROM fx_rates WHERE bank_name = ? AND iso_code = ?;'
+    params = (bank_name, iso_code)
+    cursor = connection.cursor()
+    cursor.execute(query, params)
+    row = cursor.fetchone()
+    if not row:
+        raise exceptions.FXRateNotFoundError
+    bank_name, iso_code, buy_value, sell_value = row
+    return CurrencyFXRate(
+        bank_name=bank_name,
+        iso_code=iso_code,
+        buy_value=Decimal(buy_value),
+        sell_value=Decimal(sell_value),
+    )
