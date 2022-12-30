dde37694191f282cdfcacd4cab64c581deb6b938
Eldos <eldos.baktybekov@gmail.com>
2022-12-30 19:26:25 +0600

    :recycle: Refactor configuration way

--- src/config.py
+++ src/config.py
@@ -1,28 +1,34 @@
-import os
 import pathlib
-
-import exceptions
+from configparser import ConfigParser
+from dataclasses import dataclass
 
 __all__ = (
-    'TELEGRAM_BOT_TOKEN',
-    'TELEGRAM_CHAT_IDS',
     'DATABASE_PATH',
     'SRC_DIR',
     'ROOT_PATH',
+    'load_config',
 )
-
-TELEGRAM_BOT_TOKEN = os.getenv('TELEGRAM_BOT_TOKEN')
-_chat_ids = os.getenv('TELEGRAM_CHAT_IDS')
-
-if _chat_ids is None:
-    raise exceptions.ConfigError('Environment variable "TELEGRAM_CHAT_IDS" is not set')
-if TELEGRAM_BOT_TOKEN is None:
-    raise exceptions.ConfigError('Environment variable "TELEGRAM_BOT_TOKEN" is not set')
-
-TELEGRAM_CHAT_IDS = [int(chat_id) for chat_id in _chat_ids.split(',')]
 
 ROOT_PATH = pathlib.Path(__file__).parent.parent
 
 SRC_DIR = ROOT_PATH / 'src'
 
 DATABASE_PATH = SRC_DIR / 'database.db'
+
+
+@dataclass(frozen=True, slots=True)
+class Config:
+    telegram_bot_token: str
+    chat_ids: list[int]
+
+
+def load_config(config_file_path: str | pathlib.Path) -> Config:
+    config_parser = ConfigParser()
+    config_parser.read(config_file_path)
+
+    telegram_bot = config_parser['telegram_bot']
+    token = telegram_bot['token']
+    chat_ids_raw = telegram_bot['chat_ids'].split(',')
+    chat_ids = [int(chat_id) for chat_id in chat_ids_raw if chat_id]
+
+    return Config(telegram_bot_token=token, chat_ids=chat_ids)
--- src/main.py
+++ src/main.py
@@ -1,13 +1,18 @@
-import config
+import pathlib
+
 import db
 
 import national_bank
+from config import load_config
 import utils
 from telegram import TelegramBot
 
 
 def main():
-    telegram_bot = TelegramBot(config.TELEGRAM_BOT_TOKEN)
+    config_file_path = pathlib.Path(__file__).parent.parent / 'config.ini'
+    config = load_config(config_file_path)
+
+    telegram_bot = TelegramBot(config.telegram_bot_token)
     db.init_database()
     new_fx_rates = national_bank.parse_fx_rates_xml(national_bank.get_fx_rates())
     old_fx_rates = db.get_currencies()
@@ -19,7 +24,7 @@ def main():
         if fx_rate_pairs.old.value == fx_rate_pairs.new.value:
             continue
         text = utils.format_changed_fx_rate_text(fx_rate_pairs.old, fx_rate_pairs.new)
-        for chat_id in config.TELEGRAM_CHAT_IDS:
+        for chat_id in config.chat_ids:
             telegram_bot.send_message(chat_id, text)
         db.update_currency(fx_rate_pairs.new)
 
